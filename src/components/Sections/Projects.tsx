c88fcf84516eb1a9c2f3a7df6e35def00c1888b9
Shiv Jagath <itsshivjagath@gmail.com>
2025-08-20 17:26:42 +0200

    rename image file

--- src/components/Sections/Projects.tsx
+++ src/components/Sections/Projects.tsx
@@ -5,7 +5,7 @@ import './Projects.css';
 
 // webp
 import portfolio from '../../assets/project-images/reduced/Portfolio.webp';
-import portfolio_light from '../../assets/project-images/reduced/PortfolioLight.webp';
+import portfolioLight from '../../assets/project-images/reduced/PortfolioLight.webp';
 import iTunesMediaCatalogue from '../../assets/project-images/reduced/iTunesMediaCatalogue.webp';
 import maintenanceManager from '../../assets/project-images/reduced/MaintenanceManager.webp';
 import todo from '../../assets/project-images/reduced/ToDo.webp';
@@ -20,7 +20,7 @@ export function Projects() {
           {/* Portfolio */}
           <Project
             image={portfolio}
-            lightImage={portfolio_light}
+            lightImage={portfolioLight}
             title="Portfolio"
             link="https://github.com/BigBadBodyPillow/portfolio"
             description="Portfolio description"
