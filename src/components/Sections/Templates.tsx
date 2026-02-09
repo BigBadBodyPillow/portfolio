5bf42f4fd22ba9386069477d65c04f8ad43257b1
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-02-09 13:24:57 +0200

    added buttons project to templates

--- src/components/Sections/Templates.tsx
+++ src/components/Sections/Templates.tsx
@@ -1,10 +1,12 @@
 //components
-import { Template } from './Template.tsx';
+import { Template } from "./Template.tsx";
 //css
-import './Templates.css';
+import "./Templates.css";
 
 //public images
-import eCommerce1 from '/template-images/eCommerce1.webp';
+import eCommerce1 from "/template-images/eCommerce1.webp";
+import Buttons from "/template-images/Buttons.webp";
+import ButtonsLight from "/template-images/ButtonsLight.webp";
 
 export function Templates() {
   return (
@@ -12,14 +14,33 @@ export function Templates() {
       <div className="templates-content  text-container">
         <h2 className="text-4xl font-bold mb-4 pl-1">Templates</h2>
         <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3">
+          {/* case sensitive */}
+          {/* tag options: JavaScript, HTML, CSS, Vue, React, Svelt, Angular */}
           <Template
             image={eCommerce1}
-            lightImage={''}
+            lightImage={""}
             title="eCommerce Website 1"
             link="https://github.com/BigBadBodyPillow/eCommerce-Template-1"
             description="Simple eCommerce Template."
-            tags={['Vue']}
+            tags={["Vue"]}
           ></Template>
+          <Template
+            image={Buttons}
+            lightImage={ButtonsLight}
+            title="Buttons"
+            link="https://github.com/BigBadBodyPillow/buttons"
+            description="A compilation of differnt buttons."
+            tags={["HTML", "CSS", "JavaScript"]}
+          ></Template>
+          {/* more */}
+          <div className="border border-[var(--border-colour)] rounded-lg flex justify-center items-center  bg-[rgb(235,235,235)] dark:bg-[rgb(20,20,20)] select-none cursor-not-allowed  z-1">
+            <h3 className="dark:text-[rgb(91,91,91)] text-[#858585] ">
+              more
+              <span className="dots">.</span>
+              <span className="dots">.</span>
+              <span className="dots">.</span>
+            </h3>
+          </div>
         </div>
       </div>
     </>
