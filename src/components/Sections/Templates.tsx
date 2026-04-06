bd1b7b2ff12f2a009a0a1110cc1a8eb997513d95
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-04-06 09:54:02 +0200

    removed buttons from templates

--- src/components/Sections/Templates.tsx
+++ src/components/Sections/Templates.tsx
@@ -5,8 +5,8 @@ import "./Templates.css";
 
 //public images
 import eCommerce1 from "/template-images/eCommerce1.webp";
-import Buttons from "/template-images/Buttons.webp";
-import ButtonsLight from "/template-images/ButtonsLight.webp";
+// import Buttons from "/template-images/Buttons.webp";
+// import ButtonsLight from "/template-images/ButtonsLight.webp";
 import Template2 from "/template-images/Template2.webp";
 import Template2Light from "/template-images/Template2Light.webp";
 
@@ -35,7 +35,7 @@ export function Templates() {
             description="Simple eCommerce Template."
             tags={["Vue"]}
           ></Template>
-          <Template
+          {/* <Template
             image={Buttons}
             lightImage={ButtonsLight}
             title="Buttons"
@@ -43,7 +43,7 @@ export function Templates() {
             demo="https://bigbadbodypillow.github.io/buttons/"
             description="A compilation of differnt buttons."
             tags={["HTML", "CSS", "JavaScript"]}
-          ></Template>
+          ></Template> */}
           {/* more */}
           <div className="border border-(--border-colour) rounded-lg flex justify-center items-center  bg-[rgb(235,235,235)] dark:bg-[rgb(20,20,20)] min-h-60 select-none cursor-not-allowed  z-1">
             <h3 className="dark:text-[rgb(91,91,91)] text-[#858585] ">
