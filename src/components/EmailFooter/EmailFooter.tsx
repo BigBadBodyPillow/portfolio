49f557a74267c78e8115e08e027547dba7140dd9
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-08-29 12:21:44 +0200

    make email footer not draggable

--- src/components/EmailFooter/EmailFooter.tsx
+++ src/components/EmailFooter/EmailFooter.tsx
@@ -5,7 +5,11 @@ export function EmailFooter() {
   return (
     <>
       <footer className="w-screen h-screen flex items-center justify-center dark:bg-black bg-white">
-        <a className="email" href="mailto:ItsShivJagath@gmail.com">
+        <a
+          className="email"
+          href="mailto:ItsShivJagath@gmail.com"
+          draggable="false"
+        >
           ItsShivJagath@gmail.com
         </a>
       </footer>
